11fcf9c809f8e4ec3a325525def341793fa36ac6
Anom Chakravorty <anomchakravorty3008@gmail.com>
2023-02-15 14:50:15 +0530

    modified configuration

--- functions/gpt.js
+++ functions/gpt.js
@@ -11,10 +11,10 @@ async function gpt(prompt, messageLogs, clientUserName, interactionUserName){
     try{
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `    ${clientUserName} is an AI bot created by Anom.
+            prompt: `    ${clientUserName} is a friendly Discord AI bot created by Anom.
                          ${messageLogs}
                          ${interactionUserName}: ${prompt}
-                         Type your response and dont start with "Neuron: "
+                         Type your response and dont start with Neuron: 
                    `,
             temperature: 0.5,
             max_tokens: 200
