7f5509aaae5baf6709cf2232dcb86a6b78c2a2ad
Anom Chakravorty <anomchakravorty3008@gmail.com>
2023-04-17 21:31:42 +0530

    fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,10 +5,10 @@ module.exports = {
     async execute(interaction, client) {
         if (!interaction.isChatInputCommand()) return;
 
-        if (interaction.channelId !== "894190158381985814" && interaction.channelId !== "755026338016002103") {
-            interaction.reply("Sorry you are now allowed to use me in this channel now :)\nI'm still in the development stage!\n\n If you still want to use me, please contact my creator, Anom");
-            return;
-        };
+        // if (interaction.channelId !== "894190158381985814" && interaction.channelId !== "755026338016002103") {
+        //     interaction.reply("Sorry you are now allowed to use me in this channel now :)\nI'm still in the development stage!\n\n If you still want to use me, please contact my creator, Anom");
+        //     return;
+        // };
  
         const command = interaction.client.commands.get(interaction.commandName);
  
